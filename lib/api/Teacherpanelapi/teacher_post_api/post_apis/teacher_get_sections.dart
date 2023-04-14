991f857f2fde1f015916da2f62d6c907b867bd34
locgfx <contact@locgfx.com>
2023-04-14 13:06:10 +0530

    voucher expired tr added

--- lib/api/Teacherpanelapi/teacher_post_api/post_apis/teacher_get_sections.dart
+++ lib/api/Teacherpanelapi/teacher_post_api/post_apis/teacher_get_sections.dart
@@ -7,6 +7,15 @@ import 'package:kidseau/shard_prefs/shared_prefs.dart';
 class TGetSections{
   Future<dynamic> get()async{
     String? cookie = UserPrefs.getCookies();
+    String? selectedLang = UserPrefs.getLang();
+    String langId = '';
+    if (selectedLang.toString() == 'English') {
+      langId = '0';
+    } else if (selectedLang.toString() == 'French') {
+      langId = '2';
+    } else {
+      langId = '1';
+    }
     var headers = {
       'Cookie': 'PHPSESSID=$cookie'
     };
